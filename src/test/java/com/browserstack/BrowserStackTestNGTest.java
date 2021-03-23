4209bc7d7d836a894c100d1f0550a59341a82621
Siddhartha Patki <siddhartha.p@browserstack.com>
2021-03-23 23:20:18 +0530

    updates

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -65,7 +65,7 @@ public class BrowserStackTestNGTest {
         }
 
         driver = new RemoteWebDriver(
-                new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
+                new URL("https://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
     }
 
     @AfterMethod(alwaysRun = true)
